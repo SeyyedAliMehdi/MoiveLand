c7e344f668e5edb618368c0ebd58a6ccd3faaf75
Seyyed Ali Mahdi <43001579+SeyyedAliMehdi@users.noreply.github.com>
2023-04-02 02:42:25 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
     useEffect(() => {
         searchMovies("batman");
-    }, []);
+    }, [""]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
